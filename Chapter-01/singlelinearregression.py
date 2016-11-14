029cf931a647265c4e471804d96e5bec29793d6d
莫小苝 <lovejing0306@gmail.com>
2016-11-14 16:17:49 +0800

    create chapter-01

---
+++ Chapter-01/singlelinearregression.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+from matplotlib.font_manager import FontProperties
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    font = FontProperties(fname=r'C:\windows\fonts\msyh.ttc', size=10)
