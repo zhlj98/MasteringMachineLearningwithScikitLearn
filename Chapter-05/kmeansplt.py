414617acf10dfd853a9dddce333394db1302937a
莫小苝 <lovejing0306@gmail.com>
2016-11-20 15:57:47 +0800

    create kmeansplt.py

--- Chapter-05/kmeansplt.py
+++ Chapter-05/kmeansplt.py
@@ -1,4 +1,7 @@
 # coding=utf-8
+
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -54,3 +57,24 @@ if __name__ == '__main__':
     plt.show()
 
     ## 肘部法则
+    cluster1 = np.random.uniform(0.5, 1.5, (2, 10))
+    cluster2 = np.random.uniform(3.5, 4.5, (2, 10))
+
+    X = np.hstack((cluster1, cluster2)).T
+    plt.figure()
+    plt.axis([0, 5, 0, 5])
+    plt.grid(True)
+    plt.plot(X[:, 0], X[:, 1], 'k.')
+    plt.show()
+
+    K = range(1, 10)
+    meandistortions = []
+    for k in K:
+        kmeans = KMeans(n_clusters=k)
+        kmeans.fit(X)
+        meandistortions.append(sum(np.min(cdist(X, kmeans.cluster_centers_, 'euclidean'), axis=1))/X.shape[0])
+    plt.plot(K, meandistortions, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('ji bian')
+    plt.title('best k')
+    plt.show()
